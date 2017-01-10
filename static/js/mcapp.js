cf9f10beef48b3b1b7eae854f98cdf3098d0c629
Marcos Paulo da Silva <mpsacademico@gmail.com>
2017-01-10 15:20:04 -0200

    Criação do arquivo preliminar de dados do aplicativo

---
+++ static/js/mcapp.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){	
+	$.ajax({
+		url: "data/data.json",
+		method: "GET",
+		dataType: "json"
+	}).done(function(rs){
+		var secoes = rs.secoes;
+		for (i in secoes) {			
+			$("#secoes #menu").append('<li class="table-view-cell table-view-divider">'+secoes[i].nome+'</li>');			
+			var itens = secoes[i].itens;
+			for (j in itens) {
+				$("#secoes #menu").append('<li class="table-view-cell media"><a href="'+itens[j].id+'.html" class="navigate-right"><img class="media-object pull-left img-24" src="data/img/'+itens[j].imagem+'"><div class="media-body">'+itens[j].nome+'</div></a></li>');
+			}   
+		}
+	});	
+});
