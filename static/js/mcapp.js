1d5b44276929fc59632e62755c6ac240ab106973
Marcos Paulo da Silva <mpsacademico@gmail.com>
2017-01-24 16:21:26 -0200

    Busca de dados na API do YouTube com Ajax

--- static/js/mcapp.js
+++ static/js/mcapp.js
@@ -1,9 +1,6 @@
 var itens = {};
 $(document).ready(function(){	
 });
-$("*").click(function() {  
- 	console.log($(this));
-});
 function iniciarMenuInicial(){
 	$.ajax({
 		url: "data/data.json",
@@ -16,7 +13,20 @@ function iniciarMenuInicial(){
 			itens = secoes[i].itens;
 			for (j in itens) {
 				$("#secoes #menu").append('<li class="table-view-cell media"><a class="imv" href="#playlist_'+itens[j].id+'" data-id="'+itens[j].id+'" class="navigate-right"><img class="media-object pull-left img-24" src="data/img/'+itens[j].imagem+'"><div class="media-body">'+itens[j].nome+'</div></a></li>');
-				$("#playlists").append('<div id="playlist_'+itens[j].id+'" class="playlist modal"><header class="bar bar-nav"><a class="icon icon-close pull-right" href="#playlist_'+itens[j].id+'"></a><h1 class="title">'+itens[j].nome+'</h1></header><div class="content"><div class="content-padded"><a class="zm" >saaaa</a></div></div></div>');
+				$("#playlists").append('<div id="playlist_'+itens[j].id+'" class="playlist modal"><header class="bar bar-nav"><a class="icon icon-close pull-right" href="#playlist_'+itens[j].id+'"></a><h1 class="title">'+itens[j].nome+'</h1></header><div class="content"><div class="content-padded"><ul id="videos" class="table-view"></ul></div></div></div>');
+				$.ajax({
+					url: 'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId='+itens[j].id+'&key=AIzaSyB09Y_xOjs5QFDnrbAs7tj9qzFbVAQUN04',
+					method: 'GET',
+					dataType: 'json'
+				}).done(function(rs){										
+					var items = rs.items;					
+					for (x in items) {	
+						var s = items[x].snippet;
+						var ri = s.resourceId;
+						var t = s.thumbnails;
+					    $("#videos").append('<li class="table-view-cell media"><a class="navigate-right" href="https://www.youtube.com/watch?v="'+ri.videoId+'"><img class="media-object pull-left icon img-42" src="'+t.default.url+'"><div class="media-body">'+s.title+'</div></a></li>');
+					}
+				});
 			}   
 		}
 	});
